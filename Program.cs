df25f856bcb1e0a4355233bdf3cd582c3d7d2d9f
JongD <JongD@JONGDO>
2024-06-23 20:45:59 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using BinaryTree;
+
+namespace Assignment7._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var binaryTree = new BinaryTree<int>();
+
+            binaryTree.Add(8);
+            binaryTree.Add(5);
+            binaryTree.Add(12);
+            binaryTree.Add(27);
+            binaryTree.Add(35);
+            binaryTree.Add(11);
+            binaryTree.Add(22);
+            binaryTree.Add(15);
+            binaryTree.Add(10);
+            
+            foreach (var item in binaryTree)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+
+            binaryTree.PrintAsTree();
+
+            Console.WriteLine(binaryTree.Contains(12));
+
+
+
+        }
+    }
+}
